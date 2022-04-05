e5d3954606aab937f0cbde4dd9d947a5d48f0408
mig1998 <miguelaras@hotmail.com>
2022-04-05 18:59:02 -0300

    gol

--- deploy_Unidas/src/main/java/org/unidas/security/BasicSecurityConfig.java
+++ deploy_Unidas/src/main/java/org/unidas/security/BasicSecurityConfig.java
@@ -40,8 +40,13 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		protected void configure(HttpSecurity http) throws Exception {
 
 			http.authorizeRequests()
+				.antMatchers("/**").permitAll()
 				.antMatchers("/usuarios/logar").permitAll()
 				.antMatchers("/usuarios/cadastrar").permitAll()
+				.antMatchers(HttpMethod.POST ,"/postagens").permitAll()
+		        .antMatchers(HttpMethod.POST ,"/temas").permitAll()
+		        .antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+		        .antMatchers(HttpMethod.GET ,"/temas").permitAll()
 				.antMatchers(HttpMethod.OPTIONS).permitAll()
 				.anyRequest().authenticated()
 				.and().httpBasic()
